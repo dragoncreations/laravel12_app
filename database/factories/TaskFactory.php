5079399d7224f0f76e6222deb380756d2ce63411
Dariusz Dawidowicz <ddaw@wp.pl>
2025-07-31 16:59:42 +0200

    Change due_date column in tasks table

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -22,7 +22,7 @@ class TaskFactory extends Factory
             'name' => $this->faker->sentence(), // Generates a random task name
             'description' => $this->faker->paragraph(), // Generates a random task description
             'user_id' => User::factory(), // Associates the task with a user
-            'due_date' => $this->faker->dateTimeBetween('now', '+1 month'), // Sets a random due date within the next month
+            'due_date' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'), // Sets a random due date within the next month
             'priority' => $this->faker->randomElement([Task::PRIORITY_LOW, Task::PRIORITY_MEDIUM, Task::PRIORITY_HIGH]), // Randomly assigns a priority level
             'status' => $this->faker->randomElement([Task::STATUS_TO_DO, Task::STATUS_IN_PROGRESS, Task::STATUS_DONE]), // Randomly assigns a status
             'created_at' => now(),
---
+++ database/migrations/2025_07_31_132327_change_due_date_to_date_in_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->date('due_date')->change(); // Change 'due_date' column type to date
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->timestamp('due_date')->change(); // Revert 'due_date' column type back to timestamp
+        });
+    }
+};
