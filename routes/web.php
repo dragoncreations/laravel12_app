4215aa6906999d2e686bbf2c2bd1a3dd91d351fd
Dariusz Dawidowicz <ddaw@wp.pl>
2025-07-31 12:24:22 +0200

    Redirect root URL to HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/home');
 
 Auth::routes();
 
